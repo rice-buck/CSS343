991353bcaafe892f98d19a773544392a6f6085c7
rice-buck <rhysobuckeye@outlook.com>
2026-05-31 00:08:17 -0700

    Implemented Movie and sub-classes of movie: classic, drama, and comedy

---
+++ Program4/src/Classic.cpp
@@ -0,0 +1,19 @@
+#include "Classic.h"
+#include <stdexcept>
+
+Classic::Classic(char gen, int stk, const std::string& movieT, const std::string& direc, const std::string& act, int mo, int yr)
+    : Movie(gen, stk, movieT, direc, yr), actor(act), month(mo) {
+        if(act == "") throw std::invalid_argument("Error: Actor cannot be empty!");
+
+        if(mo <= 0 || mo > 12) throw std::invalid_argument("Error: Month is out of range!");
+    }
+
+//getActor
+std::string Classic::getActor(){
+    return actor;
+}
+
+//getMonth
+int Classic::getMonth(){
+    return month;
+}
---
+++ Program4/src/Comedy.h
@@ -0,0 +1,12 @@
+#include "Movie.h"
+
+
+//comedy publicly inherits from Movie
+class Comedy : public Movie {
+
+public:
+//constructor
+Comedy(char gen, int stk, const std::string& movieT, const std::string& direc, int yr);
+
+
+};
